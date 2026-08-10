28eea82998290787e0f2d8a263f7d676dd3c30d1
Jakub Niedźwiedź <jakub.niedzwiedz@gmail.com>
2026-08-11 00:20:56 +0200

    feat(testing): create cloud image download helper

---
+++ infra/local/get_cloud_image.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+PROJECT_DIR="$(cd "$SCRIPT_DIR/../.." >/dev/null && pwd)"
+
+VM_DIR_NAME="vm"
+
+
+cd "$PROJECT_DIR" || exit
+
+mkdir -p "$VM_DIR_NAME"/images
+
+cd "$VM_DIR_NAME"/images || exit
+
+curl --fail --location --remote-name \
+"https://cloud-images.ubuntu.com/releases/noble/release/ubuntu-24.04-server-cloudimg-amd64.img"
+
+curl --fail --location --remote-name \
+"https://cloud-images.ubuntu.com/releases/noble/release/SHA256SUMS"
+
+sha256sum --check SHA256SUMS \
+  --ignore-missing
