bd5c48c52f238e8d4377622373b1b54fa2710ba8
Jakub Niedźwiedź <jakub.niedzwiedz@gmail.com>
2026-08-11 01:35:33 +0200

    refactor(scripts): create VM_DIR instead of using VM_DIR_NAME everywhere

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-VM_DIR_NAME="vm"
-export VM_DIR_NAME
+VM_DIR_NAME=vm
+
 
 PROJECT_DIR="$(cd "$SCRIPT_DIR/.." >/dev/null && pwd)"
 export PROJECT_DIR
+
+VM_DIR="${PROJECT_DIR}/${VM_DIR_NAME}"
+export VM_DIR
