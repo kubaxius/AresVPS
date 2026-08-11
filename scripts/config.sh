fa3bf07b0bb5376dacac40307f46f1eda3749d66
Jakub Niedźwiedź <jakub.niedzwiedz@gmail.com>
2026-08-11 02:17:30 +0200

    feat(scripts): define VM_NAME variable in config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 VM_DIR_NAME=vm
-
+VM_NAME="vps-local"
 
 PROJECT_DIR="$(cd "$SCRIPT_DIR/.." >/dev/null && pwd)"
-export PROJECT_DIR
+
 
 VM_DIR="${PROJECT_DIR}/${VM_DIR_NAME}"
+
+
+export VM_NAME
+export PROJECT_DIR
 export VM_DIR
